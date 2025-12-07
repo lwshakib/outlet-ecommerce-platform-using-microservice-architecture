f16aa0011fa61656c0a939d9c7c48b8894d89af7
lwshakib <leadwithshakib@gmail.com>
2025-12-07 10:46:42 +0600

    Initial commit

---
+++ apps/outlet-web/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@outlet-ecommerce-platform-using-microservice-architecture/outlet-web" />
+    </div>
+  );
+}
+
+export default App;
